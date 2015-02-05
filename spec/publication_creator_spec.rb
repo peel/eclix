6db9542017d5b37ac00e6a20c3e9a1b0914f95f5
Piotr Limanowski <piotr.limanowski@schibsted.pl>
2015-02-05 17:32:54 +0100

    spec descriptions

--- spec/publication_creator_spec.rb
+++ spec/publication_creator_spec.rb
@@ -3,34 +3,34 @@ require 'publication_creator'
 
 describe Url do
   describe '#base' do
-    it '' do
+    it 'returns a base url (http://{host}:{port}) for a given url' do
       expect(Url.new(Server.new,'abc').base).to eq('http://localhost:8080')
     end
   end
   describe '#absolute' do
-    it '' do
+    it 'returns an absolute url for a given url path' do
       expect(Url.new(Server.new,'abc').absolute).to eq('http://localhost:8080/abc')
     end
   end
 end
 describe Escenic do
   describe '#admin_url' do
-    it 'should print a valid resource form submission address' do
+    it 'returns absolute escenic admin url' do
       expect(Escenic.new.admin_url.absolute).to eq('http://localhost:8080/escenic-admin')
     end
   end
   describe '#resource_url' do
-    it 'should print a valid resource form submission address' do
+    it 'returns absolute escenic resource submission url' do
       expect(Escenic.new.resource_url.absolute).to eq('http://localhost:8080/escenic-admin/do/publication/resource')
     end
   end
   describe '#publish_url' do
-    it 'should print a valid resource form submission address' do
+    it 'returns absolute escenic publication submission url' do
       expect(Escenic.new.publish_url.absolute).to eq('http://localhost:8080/escenic-admin/do/publication/insert')
     end
   end
   describe '#publish_url' do
-    it 'should print a valid resource form submission address' do
+    it 'returns absolute publication url' do
       expect(Escenic.new.publication_url(Publication.new('demo')).absolute).to eq('http://localhost:8080/demo/')
     end
   end
